86e0293fa9d57036f25d924de4bbad9605e1fa0d
Matthew Waldron <36429088+waldronmatt@users.noreply.github.com>
2020-10-17 02:13:32 -0400

    Update callMeCell.php

--- php/callMeCell.php
+++ php/callMeCell.php
@@ -18,7 +18,7 @@
         $XML .="        </header>";
         $XML .="        <body>";
         $XML .="                <bodyContent xsi:type=\"java:com.webex.service.binding.user.SetUser\">"; 
-		$XML .="				<webExId>374510</webExId>";
+		$XML .="				<webExId>$username</webExId>";
 		$XML .="				<phones>";
 		$XML .="				<mobilePhone>$cell</mobilePhone>";
 		$XML .="				</phones>";
